1cfc0912965df1b46b3dfe387a3f58eba7374991
locr-company <r.leese@locr.com>
2024-08-20 16:41:34 +0200

    chore: Update @turf/turf dependency to version 7.1

--- src/trace.mjs
+++ src/trace.mjs
@@ -7,23 +7,23 @@
 import _ from 'lodash';
 import log from './util/log.mjs';
 import round from './util/round.mjs';
-import * as Turf from '@turf/turf';
+import * as turf from '@turf/turf';
 
 export default function trace(pgrid, d, opts, origin) {
 	/**
 	 * Filter out points not within step range
 	 */
 	log(`Filtering d=${d}...`);
-	const filtered = Turf.featureCollection(pgrid.features.filter(i => i.properties.distance <= d));
+	const filtered = turf.featureCollection(pgrid.features.filter(i => i.properties.distance <= d));
 
 	/**
 	 * Compute concave hull
 	 */
 	log(`Tracing d=${d}...`);
 	const delta = opts.hexSize > 0 ? opts.hexSize : 0.5;
-	let hull = Turf.concave(filtered, { maxEdge: delta, unit: 'kilometers' });
-	const boundingCircle = Turf.circle(origin, d, { units: 'kilometers' });
-	hull = Turf.intersect(hull, boundingCircle);
+	let hull = turf.concave(filtered, { maxEdge: delta, unit: 'kilometers' });
+	const boundingCircle = turf.circle(origin, d, { units: 'kilometers' });
+	hull = turf.intersect(turf.featureCollection([hull, boundingCircle]));
 	hull.properties.distance = d;
 
 	/**
@@ -37,9 +37,9 @@ export default function trace(pgrid, d, opts, origin) {
 	/**
 	 * Generate the appropriate bounding box and hex-grid
 	 */
-	const featureCollection = Turf.featureCollection([hull]);
-	const box = Turf.bbox(featureCollection);
-	const grid = Turf.hexGrid(box, opts.hexSize, {units: 'kilometers', mask: hull});
+	const featureCollection = turf.featureCollection([hull]);
+	const box = turf.bbox(featureCollection);
+	const grid = turf.hexGrid(box, opts.hexSize, {units: 'kilometers', mask: hull});
 	const total = grid.features.length;
 
 	/**
@@ -49,12 +49,12 @@ export default function trace(pgrid, d, opts, origin) {
 		.chain(grid.features)
 		.filter((cell, i) => {
 			log(`Fitting d=${d}: ${(i / total * 100).toFixed(2)}%`);
-			return Turf.intersect(cell, hull);
+			return turf.intersect(turf.featureCollection([cell, hull]));
 		})
 		.map(round)
 		.reduce((mem, cell, i) => {
 			log(`Merging d=${d}: ${(i / total * 100).toFixed(2)}%`);
-			return Turf.union(mem, cell);
+			return turf.union(turf.featureCollection([mem, cell]));
 		})
 		.assign({ properties: hull.properties })
 		.value();
